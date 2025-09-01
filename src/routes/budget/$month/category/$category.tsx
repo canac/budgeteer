e93368e0a6511a0afebd2a813bd0fb887f604942
Caleb Cox <canac@users.noreply.github.com>
2025-08-31 21:14:20 -0500

    Create TransactionTable component

--- src/routes/budget/$month/category/$category.tsx
+++ src/routes/budget/$month/category/$category.tsx
@@ -5,7 +5,6 @@ import {
   Drawer,
   Group,
   Stack,
-  Table,
   Text,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
@@ -15,10 +14,11 @@ import {
   useNavigate,
   useRouter,
 } from "@tanstack/react-router";
-import { format, parse, startOfMonth } from "date-fns";
+import { parse, startOfMonth } from "date-fns";
 import { DeleteCategoryModal } from "~/components/DeleteCategoryModal";
 import { EditableAmount } from "~/components/EditableAmount";
 import { EditableName } from "~/components/EditableName";
+import { TransactionTable } from "~/components/TransactionTable";
 import { getCategoryDetails } from "~/functions/getCategoryDetails";
 import { setCategoryBudgetedAmount } from "~/functions/setCategoryBudgetedAmount";
 import { setCategoryName } from "~/functions/setCategoryName";
@@ -140,52 +140,12 @@ function CategoryDetailsPage() {
             </Text>
           </Group>
         </div>
-
         <Divider />
-
-        <Stack gap="md">
-          <Text fw={500} size="lg">
-            Transactions
-          </Text>
-          <Table striped>
-            <Table.Thead>
-              <Table.Tr>
-                <Table.Th>Date</Table.Th>
-                <Table.Th>Vendor</Table.Th>
-                <Table.Th>Description</Table.Th>
-                <Table.Th ta="right">Amount</Table.Th>
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-              {categoryDetails.transactions.map((transaction) => (
-                <Table.Tr key={transaction.id}>
-                  <Table.Td>
-                    {format(new Date(transaction.date), "MMM dd")}
-                  </Table.Td>
-                  <Table.Td>{transaction.vendor}</Table.Td>
-                  <Table.Td>{transaction.description}</Table.Td>
-                  <Table.Td
-                    ta="right"
-                    c={transaction.amount < 0 ? undefined : "green"}
-                  >
-                    {formatCurrency(transaction.amount)}
-                  </Table.Td>
-                </Table.Tr>
-              ))}
-              <Table.Tr style={{ borderTop: "2px solid" }}>
-                <Table.Td>{format(date, "MMM dd")}</Table.Td>
-                <Table.Td>Starting Balance</Table.Td>
-                <Table.Td></Table.Td>
-                <Table.Td
-                  ta="right"
-                  c={categoryDetails.startingBalance < 0 ? "red" : "green"}
-                >
-                  {formatCurrency(categoryDetails.startingBalance)}
-                </Table.Td>
-              </Table.Tr>
-            </Table.Tbody>
-          </Table>
-        </Stack>
+        <TransactionTable
+          transactions={categoryDetails.transactions}
+          startingBalance={categoryDetails.startingBalance}
+          startingBalanceDate={date}
+        />
       </Stack>
       <DeleteCategoryModal
         open={deleteModalOpen}
