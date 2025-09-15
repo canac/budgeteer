c556806b4d06ccf8a0fb50548eea1ad56ba94768
Caleb Cox <canac@users.noreply.github.com>
2025-09-15 07:53:02 -0500

    Allow negative amounts

--- src/lib/transactionSchema.ts
+++ src/lib/transactionSchema.ts
@@ -1,17 +1,23 @@
 import { array, coerce, number, object, string } from "zod";
+import { roundCurrency } from "./roundCurrency";
+
+const amountSchema = number().refine((value) => value !== 0, { message: "Must not be zero" });
 
 export const transactionSchema = object({
-  amount: number(),
+  amount: amountSchema,
   vendor: string().min(1),
   description: string().optional(),
   date: coerce.date(),
   categories: array(
     object({
       categoryId: number(),
-      amount: number(),
+      amount: amountSchema,
     }),
   ).min(1),
 }).refine(
-  (value) => value.amount === value.categories.reduce((sum, category) => sum + category.amount, 0),
+  (value) =>
+    roundCurrency(
+      value.amount - value.categories.reduce((sum, category) => sum + category.amount, 0),
+    ) === 0,
   { error: "Amount must equal the sum of category amounts." },
 );
