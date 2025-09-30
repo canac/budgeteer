a6bccbe763181bbe2c6a4bf688bf28dd4b4564aa
Caleb Cox <canac@users.noreply.github.com>
2025-09-29 21:16:51 -0500

    Fix TypeScript error

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,10 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 import { monthToString } from "~/lib/monthToString";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: () =>
-    redirect({
+  beforeLoad: () => {
+    throw redirect({
       to: "/budget/$month",
       params: { month: monthToString(new Date()) },
-    }),
+    });
+  },
 });
