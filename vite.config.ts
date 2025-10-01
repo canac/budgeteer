59e64b519de5c1a1544f4f37f7c8f6f5d292e4a1
Caleb Cox <canac@users.noreply.github.com>
2025-10-01 13:57:08 -0500

    Add Netlify deployment plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
@@ -7,11 +8,5 @@ export default defineConfig({
   server: {
     port: parseInt(process.env.PORT ?? "", 10) || 3000,
   },
-  plugins: [
-    react(),
-    tsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-    tanstackStart(),
-  ],
+  plugins: [react(), tsConfigPaths({ projects: ["./tsconfig.json"] }), tanstackStart(), netlify()],
 });
