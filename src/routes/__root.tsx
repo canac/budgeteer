819c62545ea3fdba2ba24140a8c332b837e1651b
Caleb Cox <canac@users.noreply.github.com>
2026-06-15 19:51:14 -0500

    Apply theme to InputWrapper

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -46,26 +46,7 @@ const theme = createTheme({
         childrenOffset: "md",
       },
     },
-    Input: {
-      defaultProps: {
-        size: "md",
-      },
-    },
-    TextInput: {
-      defaultProps: {
-        size: "md",
-      },
-    },
-    NumberInput: {
-      defaultProps: {
-        size: "md",
-        leftSection: "$",
-        min: 0,
-        decimalScale: 2,
-        fixedDecimalScale: true,
-      },
-    },
-    PasswordInput: {
+    InputWrapper: {
       defaultProps: {
         size: "md",
       },
@@ -76,21 +57,6 @@ const theme = createTheme({
       },
     },
     Switch: {
-      defaultProps: {
-        size: "md",
-      },
-    },
-    Select: {
-      defaultProps: {
-        size: "md",
-      },
-    },
-    MultiSelect: {
-      defaultProps: {
-        size: "md",
-      },
-    },
-    Autocomplete: {
       defaultProps: {
         size: "md",
       },
